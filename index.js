fa58f7d9393d5009c08b767bf6b6acfa6e67b9b5
Jatin Kumar <your-email@example.com>
2025-06-05 20:04:33 +0530

    random jokes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// index.js
+
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
 // Array of predefined jokes
+
 const jokes = [
   "Why don’t scientists trust atoms? Because they make up everything!",
   "Why did the math book look sad? Because it had too many problems.",
@@ -12,6 +13,8 @@ const jokes = [
   "Why don’t skeletons fight each other? They don’t have the guts."
 ];
 
+app.use(express.json()); 
+
 // Root route
 app.get('/', (req, res) => {
   res.send('Welcome to the Random Jokes API! Use /api/jokes/random to get a joke.');
@@ -19,10 +22,33 @@ app.get('/', (req, res) => {
 
 // Random joke route
 app.get('/api/jokes/random', (req, res) => {
+
   const randomIndex = Math.floor(Math.random() * jokes.length);
   const randomJoke = jokes[randomIndex];
   res.json({ joke: randomJoke });
 });
+
+app.post('/api/jokes', (req, res) => {
+  try {
+   const joke = req.body.joke;
+
+   if(!joke || typeof joke !== "string"){
+  return res.status(400).json({success:false, message: "you enter wrong type or invalid joke"})
+   }
+
+   jokes.push(joke);
+   res.status(201).
+   json({success:true,
+    message:"joke added successfully",
+    totaljoke:jokes.length
+   })
+  } catch (error) {
+    res.status(500).
+    json({success:false,
+      message: error.message
+    })
+  }
+})
 
 // Start the server
 app.listen(PORT, () => {
